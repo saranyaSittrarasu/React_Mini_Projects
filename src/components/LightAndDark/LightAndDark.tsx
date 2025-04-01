62185c7776f7f776082106aa82c0b330d9ec6963
SaranyaSittrarasu <saranya.karthik@outlook.com>
2025-04-01 22:10:28 +0200

    Add light and dark theme toggle functionality

--- src/components/LightAndDark/LightAndDark.tsx
+++ src/components/LightAndDark/LightAndDark.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useLayoutEffect, useState } from "react";
 
 const LightAndDark = () => {
-  return <div>Light and Dark</div>;
+  const [theme, setTheme] = useState(localStorage.getItem("color") || "white");
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = theme;
+    document.body.style.color = theme === "white" ? "black" : "white";
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "white" ? "black" : "white";
+    setTheme(newTheme);
+    localStorage.setItem("color", newTheme);
+  };
+  return (
+    <div>
+      <h1 style={{ fontSize: "30px", marginBottom: "20px" }}>Light And Dark</h1>
+      <button
+        style={{
+          padding: "20px",
+          width: "200px",
+          backgroundColor: "lightblue",
+        }}
+        onClick={() => toggleTheme()}
+      >
+        Change Color
+      </button>
+    </div>
+  );
 };
 
 export default LightAndDark;
